334262756cdc10c3fd7ed596725e4c01dae31785
Gregory Reburn <greg@reburn.io>
2024-10-23 20:48:42 -0500

    update repo url

--- docusarus/docusaurus.config.js
+++ docusarus/docusaurus.config.js
@@ -26,7 +26,8 @@ const config = {
 				docs: {
 					sidebarPath: require.resolve("./sidebars.js"),
 					// Please change this to your repo.
-					editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+					editUrl:
+						"https://github.com/gmreburn/create-react-webext/tree/main/docusarus/docs",
 				},
 				// blog: {
 				// 	showReadingTime: true,
